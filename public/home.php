d2ff5e9de6c1eada36386d5b37de48c6b4edc4f8
jonnybeef <jonnybeef321@gmail.com>
2015-11-21 10:39:17 -0800

    Blog Entry and dispaly

---
+++ public/home.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<!--[if lt IE 7 ]><html itemscope itemtype="http://schema.org/Product" id="ie6" class="ie ie-old" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 7 ]>   <html itemscope itemtype="http://schema.org/Product" id="ie7" class="ie ie-old" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 8 ]>   <html itemscope itemtype="http://schema.org/Product" id="ie8" class="ie ie-old" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 9 ]>   <html itemscope itemtype="http://schema.org/Product" id="ie9" class="ie" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if gt IE 9]><!--><html itemscope itemtype="http://schema.org/Product" lang="en-US" prefix="og: http://ogp.me/ns#"><!--<![endif]-->
+
+<!--Blog Home Page-->
+
+    <head>
+        <title>Soccer Blog</title>
+    </head>
+
+        <main>
+            <article>
+                <header>
+                    <h1>Arsenal Football Club</h1>
+                    <p>
+                        <label>Posted by: </label>gooner_fan3309 
+                        <br>
+                        <label>Post date: </label>
+                        <time>10:00</time>
+                    </p> 
+                </header>
+                    <p>Arsenal Football Club is a professional football club based in Holloway, London. The club play in the 
+                        Premier League, the top flight of English football. One of the most successful clubs in English football, 
+                        they have won 13 First Division and Premier League titles and a record 12 FA Cups.</p>
+
+                    <p>Founded in 1886, Arsenal became the first club from the south of England to join the Football League in 1893.
+                     They won five League Championships and two FA Cups in the 1930s and became the second club to win the League and 
+                     FA Cup Double in the 20th century in the 1971. In the 1990s and 2000s, Arsenal won a Cup Double, two further League
+                     and FA Cup Doubles, went through the 2003–04 league season undefeated and became the first London club to reach the 
+                      UEFA Champions League Final.</p>
+                <footer>
+                    This is the footer.
+                    Information about this post will go here. References, tags, etc.
+                </footer>
+
+                <article>
+                    Comments and comment related content will go here. 
+                </article>
+                <nav>
+                    Important links go here
+                    <ul>
+                        <li><a href="">home</a></li>
+                        <li><a href="">related posts</a></li>
+                        <li><a href="">more stuff</a></li>
+                </nav>
+            </article>
+              <article>
+                <header>
+                    <h1>Real Madrid Club de Futbol</h1>
+                    <p>
+                        <label>Posted by: </label>99madradista 
+                        <br>
+                        <label>Post date: </label>
+                        <time>09:55</time>
+                    </p> 
+                </header>
+                    <p>Founded in 1902 as Madrid Football Club, the team has traditionally worn a white home kit since inception. 
+                       The word Real is Spanish for Royal and was bestowed to the club by King Alfonso XIII in 1920 together with the 
+                       royal crown in the emblem. The team has played its home matches in the 85,044-capacity Santiago Bernabéu Stadium 
+                       in downtown Madrid since 1947. Unlike most European sporting entities, Real Madrid's members (socios) have owned 
+                       and operated the club throughout its history.</p>
+
+                    <p>The club is the most valuable sports team in the world, worth €2.5 billion ($3.4 billion) and the world's
+                     highest-earning football club for 2013–14, with an annual revenue of €549.5 million.The club is one 
+                     of the most widely supported teams in the world. Real Madrid is one of three founding members of the Primera 
+                     División that have never been relegated from the top division, along with Athletic Bilbao and Barcelona. The club 
+                     holds many long-standing rivalries, most notably El Clásico with Barcelona and the El Derbi madrileño with Atlético 
+                     Madrid.</p>
+                <footer>
+                    This is the footer.
+                    Information about this post will go here. References, tags, etc.
+                </footer>
+
+                <article>
+                    Comments and comment related content will go here. 
+                </article>
+                <nav>
+                    Important links go here
+                    <ul>
+                        <li><a href="">home</a></li>
+                        <li><a href="">related posts</a></li>
+                        <li><a href="">more stuff</a></li>
+                </nav>
+            </article>
+        </main>
+</html>
